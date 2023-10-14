efc8c5d5eb01d076c4c6dc85b1cd028298e0fe81
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-10-14 14:21:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator">
+        <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
 
