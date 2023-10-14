779f9d991428153905d0908dbb262ceda9426329
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-10-14 14:08:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import './App.css';
-import ColorPicker from './ColorPicker'; // Import the ColorPicker component
 
-function App() {
-  // Define an array of colors to pass to the ColorPicker component
-  const colors = ['#FF5733', '#33FF57', '#3366FF', '#FFFF33', '#FF33FF'];
-
-  return (
-    <div className="App">
-      <ColorPicker colors={colors} /> {}
-    </div>
-  );
-}
-
-export default App;
